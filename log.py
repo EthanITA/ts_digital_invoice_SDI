679f92dcc5346dea56bc36f1f7c7a43640821a71
Marco <viaimperiale@gmail.com>
2021-08-17 17:11:14 +0200

    Removed log on StreamHandler

--- log.py
+++ log.py
@@ -4,7 +4,7 @@ log_name = "send_invoice.log"
 logging.basicConfig(
     handlers=[
         logging.FileHandler(log_name),
-        logging.StreamHandler()
+        # logging.StreamHandler()
     ],
     format='%(asctime)s [%(module)s/%(lineno)d] [%(levelname)-5.5s]:  %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
